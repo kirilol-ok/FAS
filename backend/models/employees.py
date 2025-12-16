6aba19bc2446cfbb1597b4f54cf4f65896efe570
damia <damian.kantor677@gmail.com>
2025-12-16 20:57:34 +0100

    some feat changes

--- backend/models/employees.py
+++ backend/models/employees.py
@@ -1,5 +1,5 @@
 from backend.models.base import CoreBase
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, func
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Boolean 
 from sqlalchemy.orm import relationship
 
 
@@ -11,6 +11,8 @@ class Employees(CoreBase):
     last_name = Column(String, index=True, nullable=False)
     email = Column(String, index=True, nullable=False, unique=True)
     qr_value = Column(String, index=True, nullable=True, unique=True)
+    dismissed = Column(Boolean, default=False)
+    dismissal_date = Column(DateTime, nullable=True)
 
     image_id = Column(Integer, ForeignKey("ImageFiles.id"), nullable=True)
     image = relationship("ImageFiles", back_populates="employees")
