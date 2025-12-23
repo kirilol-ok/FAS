8de3a02a76c591f5eda0f87e2c89f940227e7d6f
Kiryl Baranouski <kirlanby@gmail.com>
2025-12-23 14:13:58 +0100

    alembic repaired

--- backend/services/face_recognision_service.py
+++ backend/services/face_recognision_service.py
@@ -2,7 +2,20 @@ import cv2
 
 from cv2 import VideoCapture
 from dotenv import load_dotenv
+from deepface import DeepFace
+
+backends = [
+  'opencv'
+]
 
 class FaceRecogniser():
     def __init__(self):
+        return
+    def detect_face_livestream(self) -> bool:
+        DeepFace.stream(
+            db_path="database_folder",
+            detector_backend=backends[1], 
+            enable_face_analysis=True,
+            anti_spoofing=True
+                            )
         
